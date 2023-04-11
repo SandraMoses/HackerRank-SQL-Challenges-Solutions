7f1229e6c10d78be3d292071fbdc69b6f9affaa5
SandraMoses <123973369+SandraMoses@users.noreply.github.com>
2023-04-11 08:26:27 +0200

    Create Weather-Observation-Station-7.sql

---
+++ Basic Select/Weather-Observation-Station-7.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT city
+FROM station
+WHERE RIGHT(city,1) IN ('a', 'e', 'i', 'o', 'u');
+
+/*
+Another Solution:
+
+SELECT DISTINCT city
+FROM station
+WHERE city like '%a' || city like '%e' || city like '%i' || city like '%o' || city like '%u';
+
+*/
